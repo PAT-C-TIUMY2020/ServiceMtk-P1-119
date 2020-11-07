f467f4ca127ac58bf9839d86851486c0fddd1b27
Arif Budiman Arrosyid <48200892+arifbudimanarrosyid@users.noreply.github.com>
2020-11-07 14:39:05 +0700

    Praktikum 6

--- ServiceMtk-P1-20180140119/IMatematika.cs
+++ ServiceMtk-P1-20180140119/IMatematika.cs
@@ -12,15 +12,24 @@ namespace ServiceMtk_P1_20180140119
     public interface IMatematika
     {
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         /* output->*/
         int Tambah(int a, int b); //method
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kurang(int a, int b); //input
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kali(int a, int b);
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Bagi(int a, int b);
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class 
 
 
@@ -44,6 +53,13 @@ namespace ServiceMtk_P1_20180140119
         }
     }
 
+    [DataContract]
+    class MathFault{
+        [DataMember]
+        public string Kode { get; set; }
+        [DataMember]
+        public string Pesan { get; set; }
+    }
 
 
 }
--- ServiceMtk-P1-20180140119/Matematika.cs
+++ ServiceMtk-P1-20180140119/Matematika.cs
@@ -12,32 +12,80 @@ namespace ServiceMtk_P1_20180140119
     {
         public int Bagi(int a, int b)
         {
-            return a / b;
-
+            try 
+            { 
+                return a / b;
+            }
+            catch (Exception ex)
+            {
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukan salah";
+                mf.Pesan = "Masukan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
 
         public int Kali(int a, int b)
         {
-            return a * b;
+            try
+            {
+                return a * b;
+            }
+            catch (Exception ex)
+            {
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukan salah";
+                mf.Pesan = "Masukan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
 
         public int Kurang(int a, int b)
         {
-            return a - b;
-
+            try
+            {
+                return a - b;
+            }
+            catch (Exception ex)
+            {
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukan salah";
+                mf.Pesan = "Masukan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
 
         public int Tambah(int a, int b)
         {
-            return a + b;
+            try
+            {
+                return a + b;
+            }
+            catch (Exception ex)
+            {
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukan salah";
+                mf.Pesan = "Masukan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
 
         public Koordinat TKoordinat(Koordinat a, Koordinat b)
         {
-            Koordinat hasil = new Koordinat();
-            hasil.X = a.X + b.X;
-            hasil.Y = a.Y + b.Y;
-            return hasil;
+            try
+            {
+                Koordinat hasil = new Koordinat();
+                hasil.X = a.X + b.X;
+                hasil.Y = a.Y + b.Y;
+                return hasil;
+            }
+            catch (Exception ex)
+            {
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukan salah";
+                mf.Pesan = "Masukan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
     }
 }
